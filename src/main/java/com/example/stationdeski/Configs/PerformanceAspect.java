df48bb20d5aaf84e8689f8166eeb8b64360cd0e4
sahar-letaief <sahar.letaief@esprit.tn>
2023-04-17 14:25:41 +0100

    seance 11 done

--- src/main/java/com/example/stationdeski/Configs/PerformanceAspect.java
+++ src/main/java/com/example/stationdeski/Configs/PerformanceAspect.java
@@ -3,6 +3,7 @@ package com.example.stationdeski.Configs;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -18,10 +19,16 @@ public class PerformanceAspect {
     @Before("execution(* com.example.stationdeski.services.ISkieurService.*(..))")
     public void logMethodEntry(JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
-        log.info("In method " + name + " : ");
+        log.info("In method before " + name + " : ");
     }
 
-    /*@Around("execution(* com.example.stationdeski.services.*.*(..))")
+    @After("execution(* com.example.stationdeski.services.ISkieurService.*(..))")
+    public void logMethodExit(JoinPoint joinPoint) {
+        String name = joinPoint.getSignature().getName();
+        log.info("In method after " + name + " : ");
+    }
+
+   /*@Around("execution(* com.example.stationdeski.services.ISkieurService.*(..))")
     public Object profile(ProceedingJoinPoint pjp) throws Throwable {
         long start = System.currentTimeMillis();
         Object obj = pjp.proceed();
--- src/main/java/com/example/stationdeski/StationDeSkiApplication.java
+++ src/main/java/com/example/stationdeski/StationDeSkiApplication.java
@@ -2,10 +2,12 @@ package com.example.stationdeski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class StationDeSkiApplication {
 
     public static void main(String[] args) {
